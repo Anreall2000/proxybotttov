6e8d4886a152c15f58391f74c988d455c955843d
Shredinger42 <erwin.schrdinger@ya.ru>
2021-02-27 23:40:42 +0300

    API для вк

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import telebot
+import vk_api
+
 from configparser import ConfigParser
 
 cfg = ConfigParser()
 cfg.read('./config/secret.ini')
 cfg.read('./config/config.ini')
+
+vk_session = vk_api.VkApi(cfg.get('secret', 'vkToken'))
+
+
 bot = telebot.TeleBot(cfg.get('secret', 'token'))
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Ты идешь прогат?')
+    bot.send_message(message.chat.id, 'Я прогаю')
 
 
 if __name__ == '__main__':
