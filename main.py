5237413456c6aed904d5e8255a83df595a7eeab3
anreall <anreall2000@gmail.com>
2021-02-27 19:38:42 +0300

    bot init

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import telebot
 from configparser import ConfigParser
+
+cfg = ConfigParser()
+cfg.read('./config/secret.ini')
+cfg.read('./config/config.ini')
+bot = telebot.TeleBot(cfg.get('secret', 'token'))
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Ты идешь прогат?')
 
 
 if __name__ == '__main__':
-    cfg = ConfigParser()
-    cfg.read('./config/secret.ini')
-    cfg.read('./config/config.ini')
-    print(cfg.get('main', 'token'))
+    bot.polling()
