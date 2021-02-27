d383bd54694c3d1b0adcb3c1d805389c5a0b3509
Shredinger42 <erwin.schrdinger@ya.ru>
2021-02-28 00:08:38 +0300

    поправил токен

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 import vk_api
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 from configparser import ConfigParser
 
@@ -7,8 +8,9 @@ cfg = ConfigParser()
 cfg.read('./config/secret.ini')
 cfg.read('./config/config.ini')
 
-vk_session = vk_api.VkApi(cfg.get('secret', 'vkToken'))
-
+vk_session = vk_api.VkApi(token=cfg.get('secret', 'vkToken'))
+longpoll = VkBotLongPoll(vk_session, 202915868)
+vk = vk_session.get_api()
 
 bot = telebot.TeleBot(cfg.get('secret', 'token'))
 
